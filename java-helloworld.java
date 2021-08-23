5c49cff746d27e1acbe1df0f5ef021ff675a3e43
SonerBlks <88740754+SonerBlks@users.noreply.github.com>
2021-08-23 10:51:54 +0300

    Update java-helloworld.java

--- java-helloworld.java
+++ java-helloworld.java
@@ -1,5 +1,5 @@
 public class jenkins{
   public static void main(String[] args){
-    System.out.println("Hello Worldssssssssssssssssssssssssssssssssss");
+    System.out.println("Hello World!");
   }
 }
